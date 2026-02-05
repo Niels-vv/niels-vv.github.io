4a69647fb0bdb0fdce2ccc2a87c17db6286217c6
Niels-vv <28674757+Niels-vv@users.noreply.github.com>
2026-02-05 12:09:49 +0100

    Add loader display logic in openChat function

--- boostai.js
+++ boostai.js
@@ -30,8 +30,22 @@ var options = {
 };
 
 function openChat(intentID) {  
+  let loader;
+  if(intentID == 5419){
+  	loader = document.getElementById("chat-loader-1");
+  } else if (intentID == 5422) {
+    loader = document.getElementById("chat-loader-2");
+  } else if (intentID == 5425) {
+    loader = document.getElementById("chat-loader-3");
+  } else if (intentID == 5428) {
+    loader = document.getElementById("chat-loader-4");
+  }
+  loader.style.display= "block";
+
   options.chatPanel.settings.startTriggerActionId = intentID;
   options.chatPanel.settings.authStartTriggerActionId = intentID;
   var boost = window.boostInit("turesearch", options);
   boost.chatPanel.show();
+
+  loader.style.display= "none";
 }
